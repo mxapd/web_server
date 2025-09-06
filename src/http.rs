bc91be74b2d539b0b4f10dd2d912ee486cdda90b
xam <m.porseryd@gmail.com>
2025-09-06 13:07:07 +0200

    parsing a http request now fully functional altough lacking proper error handling and could use a revisit for improvments

--- src/http.rs
+++ src/http.rs
@@ -1,14 +1,20 @@
 use std::collections::HashMap;
+use std::error::Error;
+use std::io::Read;
+use std::net::TcpStream;
+use std::result::Result;
 
-pub struct HttpRequest {
-    pub method: HttpMethod,
-    pub path: String,
-    pub version: String,
-    pub headers: HashMap<String, String>,
-    pub body: Option<Vec<u8>>,
+// TODO: Error handling, returning errors. Create custom error HttpParseError
+
+struct HttpRequest {
+    method: HttpMethod,
+    path: String,
+    version: String,
+    headers: HashMap<String, String>,
+    body: Option<Vec<u8>>,
 }
 
-pub struct HttpResponse {
+struct HttpResponse {
     version: String,
     headers: HashMap<String, String>,
     body: Vec<u8>,
@@ -16,13 +22,109 @@ pub struct HttpResponse {
     // maybe no need since we can derive from status reason_phrase: String,
 }
 
-pub enum HttpMethod {
+enum HttpMethod {
     GET,
     POST,
 }
 
-pub enum HttpStatus {
+enum HttpStatus {
     Ok = 200,
     NotFound = 404,
     InternalServerError = 500,
+}
+
+pub fn handle_client(mut stream: TcpStream) -> Result<(), Box<dyn Error>> {
+    println!("Client connected: {:?}", stream.peer_addr()?);
+
+    let mut buf: Vec<u8> = Vec::new();
+    stream.read_to_end(&mut buf)?;
+
+    //println!("Recieved {} bytes from client", buf.len());
+
+    let http_request = parse_request(&buf)?;
+
+    // match http_request.path {
+    //     "/" =>
+    // }
+
+    Ok(())
+}
+
+fn split_headers_body(buffer: &[u8]) -> (&[u8], Option<&[u8]>) {
+    let separator: [u8; 4] = [0x0D, 0x0A, 0x0D, 0x0A];
+
+    if let Some(pos) = buffer
+        .windows(separator.len())
+        .position(|window| window == separator)
+    {
+        (&buffer[..pos], Some(&buffer[pos + separator.len()..]))
+    } else {
+        (buffer, None)
+    }
+}
+
+fn parse_request_line(header_data: &[u8]) -> (HttpMethod, String, String) {
+    let header_string = String::from_utf8_lossy(header_data);
+
+    println!("{}", header_string);
+
+    let header_parts = header_string
+        .split_whitespace()
+        .map(|s| s.to_string())
+        .collect::<Vec<String>>();
+
+    let http_method = match header_parts[0].as_str() {
+        "GET" => HttpMethod::GET,
+        "POST" => HttpMethod::POST,
+        _ => panic!("no match on http method"),
+    };
+
+    let http_path: String = header_parts[1].clone();
+    let http_version: String = header_parts[2].clone();
+
+    (http_method, http_path, http_version)
+}
+
+fn parse_headers(header_data: &[u8]) -> HashMap<String, String> {
+    let mut http_headers: HashMap<String, String> = HashMap::new();
+
+    let header_string = String::from_utf8_lossy(header_data);
+
+    let header_rows = header_string.split("\r\n");
+
+    let mut i = 0;
+    for row in header_rows {
+        i += 1;
+
+        if i < 2 {
+            continue;
+        }
+
+        let (key, value) = row.split_once(':').unwrap();
+        let value = value.trim_start();
+
+        http_headers.insert(String::from(key), String::from(value));
+    }
+
+    http_headers
+}
+
+fn parse_request(buffer: &Vec<u8>) -> Result<HttpRequest, Box<dyn Error>> {
+    let (header_data, body_data) = split_headers_body(&buffer.as_slice());
+
+    let body_data = body_data.map(|b| b.to_vec());
+
+    let request_line = parse_request_line(header_data);
+
+    let http_headers = parse_headers(header_data);
+
+    let http_request: HttpRequest = HttpRequest {
+        method: request_line.0,
+        path: request_line.1.clone(),
+        version: request_line.2.clone(),
+        headers: http_headers,
+        body: body_data,
+    };
+
+    Ok(http_request)
 }
--- src/main.rs
+++ src/main.rs
@@ -1,9 +1,8 @@
-use std::collections::HashMap;
-use std::io::{Read, Result};
-use std::net::{TcpListener, TcpStream};
+use std::io::Result;
+use std::net::TcpListener;
 
 mod http;
-use http::{HttpMethod, HttpRequest, HttpStatus};
+use http::handle_client;
 
 fn main() -> Result<()> {
     println!("Hello, world!");
@@ -15,102 +14,4 @@ fn main() -> Result<()> {
     }
 
     Ok(())
-}
-
-fn handle_client(mut stream: TcpStream) -> Result<()> {
-    println!("Client connected: {:?}", stream.peer_addr()?);
-
-    let mut buf: Vec<u8> = Vec::new();
-    stream.read_to_end(&mut buf)?;
-
-    //println!("Recieved {} bytes from client", buf.len());
-
-    let http_request = parse_request(&buf)?;
-
-    // match http_request.path {
-    //     "/" =>
-    // }
-
-    Ok(())
-}
-
-fn split_headers_body(buffer: &[u8]) -> (&[u8], Option<&[u8]>) {
-    let separator: [u8; 4] = [0x0D, 0x0A, 0x0D, 0x0A];
-
-    if let Some(pos) = buffer
-        .windows(separator.len())
-        .position(|window| window == separator)
-    {
-        (&buffer[..pos], Some(&buffer[pos + separator.len()..]))
-    } else {
-        (buffer, None)
-    }
-}
-
-fn parse_request_line();
-fn parse_headers();
-
-fn parse_request(buffer: &Vec<u8>) -> Result<HttpRequest> {
-    // TODO: revamp parsing, rn its splitting twice once using bytes and once after turning it into
-    // a string. Would like byte split to get headers, body. then turn headers into string or vec
-    // to continue parsing
-
-    // TODO: Split into smaller functions
-
-    // TODO: Error handling, returning errors. Create custom error HttpParseError
-
-    dbg!(&body_data);
-
-    let buffer_string = String::from_utf8_lossy(buffer);
-
-    println!("{}", buffer_string);
-
-    let (header_part, body_part) = buffer_string.split_once("\r\n\r\n").unwrap();
-
-    let header_parts = header_part
-        .split_whitespace()
-        .map(|s| s.to_string())
-        .collect::<Vec<String>>();
-
-    let http_method: HttpMethod;
-
-    // GET METHOD PART
-    match header_parts[0].as_str() {
-        "GET" => http_method = HttpMethod::GET,
-        "POST" => http_method = HttpMethod::POST,
-        _ => panic!("no match on http method"),
-    }
-
-    let http_path: &String = &header_parts[1];
-    let http_version: &String = &header_parts[2];
-
-    // loop trough the rest and map header keys and values
-
-    let mut http_headers: HashMap<String, String> = HashMap::new();
-
-    let header_rows = header_part.split('\n');
-
-    let mut i = 0;
-    for row in header_rows {
-        i += 1;
-
-        if i < 2 {
-            continue;
-        }
-
-        let (key, value) = row.split_once(':').unwrap();
-        let value = value.trim_start();
-
-        http_headers.insert(String::from(key), String::from(value));
-    }
-
-    let http_request: HttpRequest = HttpRequest {
-        method: http_method,
-        path: http_path.clone(),
-        version: http_version.clone(),
-        headers: http_headers,
-        body: body_data,
-    };
-
-    Ok(http_request)
 }
