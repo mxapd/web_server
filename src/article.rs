20f720bc2b3f9e58e9f66225e0a6bd2073004d84
xam <m.porseryd@gmail.com>
2025-09-13 15:46:25 +0200

    dmsnaikpdnaspodas

---
+++ src/article.rs
@@ -0,0 +1,8 @@
+struct article {
+    title: String,
+    url: String,
+    source: String,
+    published_date: String,
+    content: String,
+    summary: String,
+}
---
+++ src/database_actor.rs
@@ -0,0 +1,11 @@
+// event loop for handling incoming messages
+//
+
+pub struct PSQLActor {}
+
+impl PSQLActor {
+    //pub fn new(stream: TcpStream) -> Self {
+    //}
+
+    //pub fn run(self) {}
+}
